7b5e1b75cd9725cf43345c2c07559751923a0995
Hszoo <trre1827151@gmail.com>
2024-12-17 20:05:17 +0900

    Initial Commit

---
+++ likelion12th_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  likelion12th_app
+//
+//  Created by 홍성주 on 12/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ likelion12th_appTests/likelion12th_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  likelion12th_appTests.swift
+//  likelion12th_appTests
+//
+//  Created by 홍성주 on 12/17/24.
+//
+
+import Testing
+@testable import likelion12th_app
+
+struct likelion12th_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
